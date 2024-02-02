1bc9030bd3f14d38e1400f415a44d658783ce488
Ed Jones <ed.jones@canonical.com>
2024-02-02 18:50:51 +0000

    Simplify bound declarations

--- starform/extensionset.go
+++ starform/extensionset.go
@@ -2,20 +2,54 @@ package starform
 
 import (
 	"fmt"
-	"math"
 	"sort"
 
 	"github.com/canonical/starlark/starlark"
-	"github.com/canonical/starlark/syntax"
 )
 
 type ExtensionSet struct {
-	starlarkOptions     syntax.FileOptions
 	printHandler        func(thread *starlark.Thread, msg string) // FIXME non so se mi piace
 	loader              ScriptLoader
 	cache               ScriptCache
 	flags               starlark.SafetyFlags
 	maxAllocs, maxSteps uint64
+}
+
+type ExtensionSetOptions struct {
+	PrintHandler        func(thread *starlark.Thread, msg string) // FIXME non so se mi piace
+	Loader              ScriptLoader
+	Flags               starlark.SafetyFlags
+	MaxAllocs, MaxSteps uint64
+	Cache               ScriptCache
+}
+
+func NewExtensionSet(options ExtensionSetOptions) (*ExtensionSet, error) {
+	if options.Loader == nil {
+		return nil, fmt.Errorf("Loader cannot be nil")
+	}
+
+	result := &ExtensionSet{
+		printHandler: options.PrintHandler,
+		loader:       options.Loader,
+		maxAllocs:    options.MaxAllocs,
+		maxSteps:     options.MaxSteps,
+		flags:        options.Flags,
+		cache:        options.Cache,
+	}
+	if result.cache == nil {
+		result.cache = DefaultScriptCache
+	}
+	return result, nil
+}
+
+func (es *ExtensionSet) makeThread() *starlark.Thread {
+	thread := &starlark.Thread{
+		Print: es.printHandler,
+	}
+	thread.RequireSafety(es.flags)
+	thread.SetMaxSteps(es.maxSteps)
+	thread.SetMaxAllocs(es.maxAllocs)
+	return thread
 }
 
 func (es *ExtensionSet) Load(name string) (*Extension, error) {
@@ -61,52 +95,4 @@ func (es *ExtensionSet) Load(name string) (*Extension, error) {
 		Name:    name,
 		modules: modules,
 	}, nil
-}
-
-type ExtensionSetOptions struct {
-	PrintHandler        func(thread *starlark.Thread, msg string) // FIXME non so se mi piace
-	LanguageOptions     syntax.FileOptions
-	Flags               starlark.SafetyFlags
-	MaxAllocs, MaxSteps *uint64
-	Loader              ScriptLoader
-	Cache               ScriptCache
-}
-
-func NewExtensionSet(options ExtensionSetOptions) (*ExtensionSet, error) {
-	if options.Loader == nil {
-		return nil, fmt.Errorf("Loader cannot be nil")
-	}
-
-	result := &ExtensionSet{
-		printHandler:    options.PrintHandler,
-		starlarkOptions: options.LanguageOptions,
-		flags:           options.Flags,
-		maxAllocs:       math.MaxUint64,
-		maxSteps:        math.MaxUint64,
-		loader:          options.Loader,
-	}
-
-	result.cache = options.Cache
-	if result.cache == nil {
-		result.cache = DefaultScriptCache
-	}
-
-	if options.MaxAllocs != nil {
-		result.maxAllocs = *options.MaxAllocs
-	}
-	if options.MaxSteps != nil {
-		result.maxSteps = *options.MaxSteps
-	}
-
-	return result, nil
-}
-
-func (es *ExtensionSet) makeThread() *starlark.Thread {
-	thread := &starlark.Thread{
-		Print: es.printHandler,
-	}
-	thread.RequireSafety(es.flags)
-	thread.SetMaxSteps(es.maxSteps)
-	thread.SetMaxAllocs(es.maxAllocs)
-	return thread
 }
