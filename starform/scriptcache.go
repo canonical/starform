d350017c0d141c814fba21501335f8a5497b0793
Marco Manino <mrcmnn@gmail.com>
2024-02-27 16:42:24 +0100

    sealed instead of private

--- starform/scriptcache.go
+++ starform/scriptcache.go
@@ -1,11 +1,11 @@
 package starform
 
 type ScriptCache interface {
-	private() // This will be removed once this interface is stabilised.
+	sealed() // This will be removed once this interface is stable.
 }
 
 type noopScriptCache struct{}
 
-func (*noopScriptCache) private() {}
+func (*noopScriptCache) sealed() {}
 
 var NoopScriptCache ScriptCache = &noopScriptCache{}
