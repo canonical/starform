6333794722bba0fe0220bd062764f52883340203
Ed Jones <ed.jones@canonical.com>
2024-03-01 19:15:44 +0000

    Remove unnecessary comment

--- starform/extensionset.go
+++ starform/extensionset.go
@@ -200,7 +200,6 @@ func checkLoadPath(path string) (err error) {
 			case '_':
 				return errors.New(`path has component which starts with "_"`)
 			case '/':
-				// Precondition: path is not absolute.
 				return errors.New(`path contains "//"`)
 			}
 		case '.':
