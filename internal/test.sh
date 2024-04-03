d7d0fab7076552800e9c0391f3a95354a167c99c
Marco Manino <mrcmnn@gmail.com>
2024-04-03 16:20:20 +0200

    Use GOPRIVATE

--- internal/test.sh
+++ internal/test.sh
@@ -2,5 +2,5 @@
 
 set -eu
 
-git config --global url."ssh://git@github.com/".insteadOf https://github.com/        
-go test ./...
+# git config --global url."ssh://git@github.com/".insteadOf https://github.com/        
+GOPRIVATE=github.com/canonical/starlark go test ./...
