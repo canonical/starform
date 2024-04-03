af07593ac5bad2c174b2e648dce869ac020d3288
Marco Manino <mrcmnn@gmail.com>
2024-04-03 16:21:43 +0200

    Using https instead of ssh

--- internal/test.sh
+++ internal/test.sh
@@ -2,5 +2,5 @@
 
 set -eu
 
-# git config --global url."ssh://git@github.com/".insteadOf https://github.com/        
+git config --global url."ssh://git@github.com/".insteadOf https://github.com/        
 GOPRIVATE=github.com/canonical/starlark go test ./...
