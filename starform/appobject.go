965ca8468f68524e0d0ecdaa299ccac1c1311317
Ed Jones <ed.jones@canonical.com>
2024-04-11 19:57:01 +0100

    Add `AppObject` documentation

--- starform/appobject.go
+++ starform/appobject.go
@@ -6,6 +6,8 @@ import (
 	"github.com/canonical/starlark/starlark"
 )
 
+// AppObject is the common point for exposing the current state of the
+// application into Starlark and for Starlark to declare intents.
 type AppObject struct {
 	name string
 }
