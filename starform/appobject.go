5781ccce6f305d9ccb476c37d43536867ef4a11a
Ed Jones <ed.jones@canonical.com>
2024-04-12 12:30:03 +0100

    Add `SafeString` implementation

--- starform/appobject.go
+++ starform/appobject.go
@@ -17,6 +17,7 @@ func NewAppObject(name string) *AppObject {
 }
 
 var _ starlark.Value = &AppObject{}
+var _ starlark.SafeStringer = &AppObject{}
 
 func (ao *AppObject) String() string       { return ao.name }
 func (ao *AppObject) Type() string         { return ao.name }
@@ -24,4 +25,12 @@ func (ao *AppObject) Freeze()              {}
 func (ao *AppObject) Truth() starlark.Bool { return true }
 func (ao *AppObject) Hash() (uint32, error) {
 	return 0, fmt.Errorf("unhashable type: %s", ao.Type())
+}
+func (ao *AppObject) SafeString(thread *starlark.Thread, sb starlark.StringBuilder) error {
+	if err := starlark.CheckSafety(thread, starlark.CPUSafe|starlark.MemSafe|starlark.TimeSafe|starlark.IOSafe); err != nil {
+		return err
+	}
+
+	_, err := sb.WriteString(ao.String())
+	return err
 }
--- starform/appobject_test.go
+++ starform/appobject_test.go
@@ -1,10 +1,17 @@
 package starform_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/canonical/starform/starform"
+	"github.com/canonical/starlark/starlark"
+	"github.com/canonical/starlark/startest"
 )
+
+func isStarlarkCancellation(err error) bool {
+	return strings.Contains(err.Error(), "Starlark computation cancelled:")
+}
 
 func TestAppObjectAsStarlarkValue(t *testing.T) {
 	const appObjectName = "testObject"
@@ -24,4 +31,63 @@ func TestAppObjectAsStarlarkValue(t *testing.T) {
 	if _, err := appObject.Hash(); err == nil {
 		t.Errorf("app object should not be hashable")
 	}
+}
+
+func TestAppObjectSafeString(t *testing.T) {
+	const appObjectName = "testObject"
+
+	t.Run("nil-thread", func(t *testing.T) {
+		defer func() {
+			if err := recover(); err != nil {
+				t.Errorf("unexpected panic: %s", err)
+			}
+		}()
+
+		appObject := starform.NewAppObject(appObjectName)
+		sb := &strings.Builder{}
+		appObject.SafeString(nil, sb)
+		if str := sb.String(); str != appObjectName {
+			t.Error("invalid SafeString value")
+		}
+	})
+
+	t.Run("regular-operation", func(t *testing.T) {
+		st := startest.From(t)
+		st.RequireSafety(starlark.MemSafe | starlark.CPUSafe | starlark.IOSafe)
+		st.SetMaxSteps(uint64(len(appObjectName)))
+		st.RunThread(func(thread *starlark.Thread) {
+			appObject := starform.NewAppObject(appObjectName)
+			for i := 0; i < st.N; i++ {
+				sb := starlark.NewSafeStringBuilder(thread)
+				if err := appObject.SafeString(thread, sb); err != nil {
+					st.Error(err)
+				}
+				if err := sb.Err(); err != nil {
+					st.Error(err)
+				}
+				if err := thread.AddAllocs(starlark.StringTypeOverhead); err != nil {
+					st.Error(err)
+				}
+				st.KeepAlive(sb.String())
+			}
+		})
+	})
+
+	t.Run("cancellation", func(t *testing.T) {
+		st := startest.From(t)
+		st.RequireSafety(starlark.TimeSafe)
+		st.SetMaxSteps(0)
+		st.RunThread(func(thread *starlark.Thread) {
+			thread.Cancel("done")
+			appObject := starform.NewAppObject(appObjectName)
+			for i := 0; i < st.N; i++ {
+				sb := starlark.NewSafeStringBuilder(thread)
+				if err := appObject.SafeString(thread, sb); err == nil {
+					st.Error("expected cancellation")
+				} else if !isStarlarkCancellation(err) {
+					st.Errorf("expected cancellation, got %v", err)
+				}
+			}
+		})
+	})
 }
