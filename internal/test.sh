782cd464cf85565c5ff44976ebd286d01331ee61
Marco Manino <mrcmnn@gmail.com>
2024-04-15 08:40:06 +0200

    Using user

--- internal/test.sh
+++ internal/test.sh
@@ -2,5 +2,5 @@
 
 set -eu
 
-git config --global url."https://$GITHUB_TOKEN:@github.com/".insteadOf https://github.com/        
+git config --global url."https://$GITHUB_USER:$GITHUB_TOKEN@github.com/".insteadOf https://github.com/        
 GOPRIVATE=github.com/canonical/starlark go test ./...
