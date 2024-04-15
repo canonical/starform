d9b2bfa55b50e2057ed879d0a6f7a969f05b497e
Marco Manino <mrcmnn@gmail.com>
2024-04-15 09:08:17 +0200

    Some more testing

--- internal/test.sh
+++ internal/test.sh
@@ -2,5 +2,20 @@
 
 set -eu
 
-git config --global url."https://x-access-token:$GITHUB_TOKEN@github.com/".insteadOf https://github.com/        
-GOPRIVATE=github.com/canonical/starlark go test ./...
+echo "Trying to clone with all possibilities:" 
+false || \
+    git clone https://$GITHUB_TOKEN@github.com/canonical/starlark.git || \
+    git clone https://$GITHUB_TOKEN:@github.com/canonical/starlark.git || \
+    git clone https://oauth2:$GITHUB_TOKEN@github.com/canonical/starlark.git || \
+    git clone https://x-access-token:$GITHUB_TOKEN@github.com/canonical/starlark.git || \
+    git clone https://oauth2:$GITHUB_TOKEN@github.com/canonical/starlark.git || 
+    echo "All failed! :-("
+
+
+echo "Now trying with netrc"
+
+echo "machine github.com login $GITHUB_USER password $GITHUB_TOKEN" > ~/.netrc
+git clone https://github.com/canonical/starlark.git 
+
+# git config --global url."https://x-access-token:$GITHUB_TOKEN@github.com/".insteadOf https://github.com/        
+# GOPRIVATE=github.com/canonical/starlark go test ./...
