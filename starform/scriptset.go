c9cecae845320bdecdc3dafa83e57a97ceb605ce
kcza <ed.jones@canonical.com>
2024-04-19 08:50:17 +0100

    Use UK-English spelling of 'cancelled' (#19)

--- starform/scriptset.go
+++ starform/scriptset.go
@@ -108,7 +108,7 @@ func (ss *ScriptSet) LoadSources(ctx context.Context, sources []ScriptSource) er
 	go func() {
 		select {
 		case <-ctx.Done():
-			thread.Cancel("operation canceled")
+			thread.Cancel("operation cancelled")
 		case <-done:
 		}
 	}()
