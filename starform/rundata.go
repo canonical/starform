af5811d5261a6cf29fdda32a9b8c3e1a0de5f487
Ed Jones <ed.jones@canonical.com>
2024-05-23 18:25:11 +0100

    Change local key

--- starform/rundata.go
+++ starform/rundata.go
@@ -12,7 +12,7 @@ type runData struct {
 	eventName string // TODO(kcza): Generalise this to include some ID for more efficient comparison.
 }
 
-const runDataLocalKey = "starform.runData"
+const runDataLocalKey = "starform-run-data"
 
 var errRunDataMissing = fmt.Errorf("%s missing", runDataLocalKey)
 
