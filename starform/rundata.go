39855d04f9abcbc3dfdf04b239415bba0f3bc1f5
kcza <ed.jones@canonical.com>
2024-06-06 16:05:31 +0100

    Tweak RunData local storage (#23)

--- starform/rundata.go
+++ starform/rundata.go
@@ -12,9 +12,9 @@ type runData struct {
 	eventName string // TODO(kcza): Generalise this to include some ID for more efficient comparison.
 }
 
-const runDataLocalKey = "starform.runData"
+const runDataLocalKey = "starform-run-data"
 
-var errRunDataMissing = fmt.Errorf("%s missing", runDataLocalKey)
+var errRunDataMissing = errors.New("starform internal data missing")
 
 func getRunData(thread *starlark.Thread) (*runData, error) {
 	ret, ok := thread.Local(runDataLocalKey).(*runData)
