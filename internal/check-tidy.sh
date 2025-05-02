92a3c3c8d6a9dc66b3091114ae5d43fdc49a6681
kcza <ed@kcza.net>
2025-05-02 14:46:17 +0100

    Reintroduce `GOPRIVATE` declarations

--- internal/check-tidy.sh
+++ internal/check-tidy.sh
@@ -9,7 +9,7 @@ cleanup() {
 
 cp go.mod go.mod.untidy
 cp go.sum go.sum.untidy
-go mod tidy
+GOPRIVATE=github.com/canonical/ go mod tidy
 diff -w go.mod.untidy go.mod || { echo "go.mod is not tidy"; cleanup; exit 1; }
 diff -w go.sum.untidy go.sum || { echo "go.sum is not tidy"; cleanup; exit 1; }
 cleanup
